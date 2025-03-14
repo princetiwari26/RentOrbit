ff6821b80a2fe0c0dd01701c7250be74a5a831ad
Prince Tiwari <princetiwari.profes@gmail.com>
2025-03-14 20:33:23 +0530

    frontend

--- house-hunt/src/App.js
+++ house-hunt/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='bg-blue-500 text-white px-4 py-2'>House Hunt</h1>
     </div>
   );
 }
