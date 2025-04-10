3fe3a59524ba5975a5b959fe4a34562a7f576876
Prince Tiwari <princetiwari.profes@gmail.com>
2025-04-10 20:55:39 +0530

    backend

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUE_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
 });
